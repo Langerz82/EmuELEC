1652ed51b6028c72255bd3a1a0f4494da99af045
Joshua L <Langerz82@users.noreply.github.com>
2024-03-07 16:18:52 +1100

    initial commit

---
+++ packages/sx05re/emuelec/bin/emuelecRunApp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
+
+# Source predefined functions and variables
+. /etc/profile
+
+# DO NOT modify this file, if you need to use autostart please use /storage/.config/custom_start.sh 
+
+shift
+
+emuelec-utils init_app_video
+
+eval $@
+
+emuelec-utils end_app_video
