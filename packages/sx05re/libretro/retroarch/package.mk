76c41f2d2d2d8db3d9090434ef864f42e5288e08
Joshua L <Langerz82@users.noreply.github.com>
2024-11-09 08:37:44 +1100

    initial commit.

--- packages/sx05re/libretro/retroarch/package.mk
+++ packages/sx05re/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="1e1b24c35996436e6cf0fe50ec046e2e03a0dbfa"
+PKG_VERSION="c752312a19b9e32042e924aab8b9b2244cf6c20d"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="${PKG_SITE}.git"
 PKG_LICENSE="GPLv3"
