cb9092682c25ca89990f8617610ce4bb60ee0c9d
Joshua L <Langerz82@users.noreply.github.com>
2025-10-16 16:20:38 +1100

    initial commit.

--- packages/sx05re/emuelec/bin/joy_common.sh
+++ packages/sx05re/emuelec/bin/joy_common.sh
@@ -15,111 +15,69 @@ EMULATOR="${1}"
 
 mkdir -p "/tmp/jc"
 
-DEBUG_FILE="/tmp/jc/${EMULATOR}_joy_debug.cfg"
-CACHE_FILE="/tmp/jc/${EMULATOR}_joy_cache.cfg"
+#DEBUG_FILE="/tmp/jc/${EMULATOR}_joy_debug.cfg"
+#CACHE_FILE="/tmp/jc/${EMULATOR}_joy_cache.cfg"
 
+SDLJOYTEST="/tmp/jc/sdljoytest.txt"
+
+jc_get_device_header() {
+  local GUID="${1}"
+
+  local v=${GUID:0:8}
+  local bus=$(echo ${v:2:2}${v:0:2}) # Bus, generally not needed
+  v=${GUID:8:8}
+  local vendor=$(echo ${v:2:2}${v:0:2}) # Vendor
+  v=${GUID:16:8}
+  local product=$(echo ${v:2:2}${v:0:2}) # Product
+  v=${GUID:24:8}
+  local version=$(echo ${v:2:2}${v:0:2}) # Version
+  echo "^I:.*Bus=${bus} Vendor=${vendor} Product=${product} Version=${version}$"
+}
+
+jc_get_device() {
+  local I_REGEX=$( jc_get_device_header ${2} )
+  for (( pi = ${1}; pi < 9; pi++ )); do
+    local EE_DEVICE=$( cat /proc/bus/input/devices | grep -Ew -A 5 "$I_REGEX" | grep -E -B 5 "^[ ]*H: Handlers=.*js${pi}.*$" )
+    [[ -z "${EE_DEVICE}" ]] && continue
+    local EE_NAME=$( echo ${EE_DEVICE} | grep "N: Name=" | cut -d'"' -f2 )
+    local EE_JSNUM=$( echo ${EE_DEVICE} | grep "H: Handlers=" |  sed -nE 's|^.*(js[0-9]+).*$|\1|p' )
+    echo ${EE_JSNUM} \"${EE_NAME}\"
+    return
+  done
+}
+
+jc_get_config() {
+  local JOY_NAME=$( cat ${SDLJOYTEST} | grep "Joystick ${1} name" | cut -d"'" -f2 )
+  [[ -z ${JOY_NAME} ]] && return
+
+  local DEVICE_GUID=$( cat ${SDLJOYTEST} | grep "Joystick ${1} Guid" | cut -d" " -f4 )
+  [[ -z ${DEVICE_GUID} ]] && return
+
+  local JOYMAPPING=$( cat ${SDLJOYTEST} | grep "mapping: ${DEVICE_GUID}" | cut -d" " -f7- )
+  [[ -z ${JOYMAPPING} ]] && return
+
+  local JC_DEVICE=$( jc_get_device ${1} ${DEVICE_GUID} )
+  [[ -z ${JC_DEVICE} ]] && return
+
+  local JSI=$( echo ${JC_DEVICE} | cut -d' ' -f1 )
+  [[ -z ${JSI} ]] && JSI=js0
+
+  local JOY_UDEVNAME=$( echo ${JC_DEVICE} | cut -d'"' -f2 )
+
+  local PLAYER_CFG="$(( $1 + 1 )) ${JSI} ${DEVICE_GUID} \"${JOY_NAME}\" \"${JOYMAPPING}\" \"${JOY_UDEVNAME}\""
+  echo ${PLAYER_CFG}
+}
 
 jc_get_players() {
-# You can set up to 8 player on ES
-  declare -i PLAYER=1
+  sdljoytest -skip_loop > ${SDLJOYTEST}
 
-# Dump gamepad information
-  cat /proc/bus/input/devices \
-    | grep -B 5 -A 3 -P "H: Handlers=(?=.*?js[0-9])(?=.*?event[0-9]+).*$" \
-    | grep -Ew -B 8 "B: KEY=[0-9a-f ]+" > /tmp/input_devices
-
-# Determine how many gamepads/players are connected
-  JOYS=$(ls -ltr /dev/input/js* | awk '{print $8"\t"$9"\t"$10}' | sort \
-    | awk '{print $3}' | cut -d'/' -f4)
-  if [[ -f "/storage/.config/JOY_LEGACY_ORDER" ]]; then
-    JOYS=$(ls -A1 /dev/input/js*| sort | sed "s|/dev/input/||g")
-  fi
-
-  declare -a PLAYER_CFGS=()
-
-  for dev in $(echo ${JOYS}); do
-    local JSI=${dev}
-    local DETAILS=$(cat /tmp/input_devices | grep -P "H: Handlers(?=.*?[= ]${JSI} )" -B 5)
-
-    local PROC_GUID=$(echo "${DETAILS}" | grep I: | sed "s|I:\ Bus=||" | sed "s|\ Vendor=||" | sed "s|\ Product=||" | sed "s|\ Version=||")
-    local DEVICE_GUID=$(jc_generate_guid $((PLAYER-1)))
-    [[ -z "${DEVICE_GUID}" ]] && continue
-
-    local GC_CONFIG=$(cat "${GCDB}" | grep "${DEVICE_GUID}" | grep "platform:Linux" | head -1)
-    echo "GC_CONFIG=${GC_CONFIG}"
-    [[ -z ${GC_CONFIG} ]] && continue
-
-    local JOY_NAME=$(echo "${DETAILS}" | grep -E "^N: Name.*[\= ]?.*$" | cut -d "=" -f 2 | tr -d '"')
-    [[ -z "${JOY_NAME}" ]] && continue
-
-    # Add the joy config to array if guid and joyname set.
-    if [[ ! -z "${DEVICE_GUID}" && ! -z "${JOY_NAME}" ]]; then
-      local PLAYER_CFG="${JSI} ${DEVICE_GUID} \"${JOY_NAME}\""
-      PLAYER_CFGS[$((PLAYER-1))]="${PLAYER_CFG}"
-      ((PLAYER++))
-    fi
-  done
-
-  rm /tmp/input_devices
-
-  if [[ -z "${PLAYER_CFGS[@]}" ]]; then
-    echo "NO GAME CONTROLLERS WERE DETECTED."
-    return
-  fi
-
-  local cfgCount=${#PLAYER_CFGS[@]}
-  MANUAL_CONFIG=$(cat "/tmp/controllerconfig.txt")
-  echo "MANUAL_CONFIG=${MANUAL_CONFIG}"
-  if [[ ! -z "${MANUAL_CONFIG}" && ! -f "/storage/.config/EE_CONTROLLER_OVERIDE_OFF" ]]; then
-    declare -a GUID_ORDER=(${MANUAL_CONFIG})
-    declare -a GUIDS=()
-
-    local index=0
-    local row=0
-    local i=0
-    local pl=0
-    for i in ${GUID_ORDER[@]}; do
-      row=$(( index % 4 ))
-      [[ ${row} == 0 ]] && pl=${i:2:1}
-      [[ ${row} == 3 ]] && GUIDS[${pl}]=${GUID_ORDER[$(( index+0 ))]}
-      (( index++ ))
-    done
-    echo "GUID_ORDER=${GUIDS[@]}"
-    local si=0
-    for (( i=1; i<=${cfgCount}; i++ )); do
-      local tGUID=${GUIDS[i]}
-      [[ "${tGUID}" == "" ]] && continue
-      for (( j=${si}; j<${cfgCount}; j++ )); do
-        local cfgGUID=$(echo "${PLAYER_CFGS[${j}]}" | cut -d' ' -f2)
-        if [[ ${tGUID} == ${cfgGUID} ]]; then
-          local tmp="${PLAYER_CFGS[${j}]}"
-          PLAYER_CFGS[${j}]="${PLAYER_CFGS[${si}]}"
-          PLAYER_CFGS[${si}]="${tmp}"
-          (( si++ ))
-          break
-        fi
-      done
-    done
-  fi
-
-  local PLAYER_CFG=
-  mkdir -p /tmp/JOYPAD_NAMES
-  rm /tmp/JOYPAD_NAMES/*.txt 2>/dev/null
-  for p in {1..4}; do
-    local CFG="${p} ${PLAYER_CFGS[$(( p-1 ))]}"
-    if [[ ${p} -le ${cfgCount} ]]; then
-      echo "PLAYER_CFG=${CFG}"
-    fi
-    eval clean_pad ${CFG}
-    if [[ "${CFG}" != "${p} " ]]; then
-      local JOYNAME=$(echo "${CFG}" | cut -d' ' -f4-)
-      echo "${JOYNAME}" > "/tmp/JOYPAD_NAMES/JOYPAD${p}.txt"
-      eval set_pad ${CFG}
-    fi
+  for jci in {0..3}; do
+    CFG=$( jc_get_config "${jci}" )
+    [[ -z "${CFG}" ]] && CFG=$(( $jci + 1 ))
+    echo ${CFG}
+#    eval clean_pad ${CFG}
+#    eval set_pad ${CFG}
   done
 }
 
-jc_generate_guid() {
-  local GUID_INDEX=${1}
-  echo $(sdljoytest -skip_loop | grep "Joystick ${GUID_INDEX} Guid" | cut -d' ' -f 4)
-}
+jc_get_players
