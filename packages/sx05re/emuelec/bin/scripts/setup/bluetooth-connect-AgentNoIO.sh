45f5a5942ce57681f08cfd2672eb1a7e34f51d6b
Joshua L <Langerz82@users.noreply.github.com>
2026-01-19 17:52:14 +1100

    gptokeyb close feature.

--- packages/sx05re/emuelec/bin/scripts/setup/bluetooth-connect-AgentNoIO.sh
+++ packages/sx05re/emuelec/bin/scripts/setup/bluetooth-connect-AgentNoIO.sh
@@ -6,8 +6,15 @@
 # Source predefined functions and variables
 . /etc/profile
 
+PROC_NAME="ee-bt-nino"
+
+ee_console disable
 ee_console enable
 
-emuelec-bluetooth-NoInputNoOutput
+gptokeyb -1 "${PROC_NAME}" -killsignal 15 && pkill emuelec-bluetoo && pkill gptokeyb &
+
+bash -c "exec -a ${PROC_NAME} emuelec-bluetooth-NoInputNoOutput" && pkill gptokeyb &
+
+wait
 
 exit 0
