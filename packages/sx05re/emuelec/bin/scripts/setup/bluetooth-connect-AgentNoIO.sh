0d730a3929d1be9c3b79d43da8eb502f6d1e8a62
Joshua L <Langerz82@users.noreply.github.com>
2026-01-20 14:28:33 +1100

    Refactor bluetooth-connect-AgentNoIO.sh script with gptokeyb update.

--- packages/sx05re/emuelec/bin/scripts/setup/bluetooth-connect-AgentNoIO.sh
+++ packages/sx05re/emuelec/bin/scripts/setup/bluetooth-connect-AgentNoIO.sh
@@ -6,15 +6,13 @@
 # Source predefined functions and variables
 . /etc/profile
 
-PROC_NAME="ee-bt-nino"
-
 ee_console disable
 ee_console enable
 
-gptokeyb -1 "${PROC_NAME}" -killsignal 15 && pkill emuelec-bluetoo && pkill gptokeyb &
+gptokeyb -customkill "pkill emuelec-bluetoo" &
 
-bash -c "exec -a ${PROC_NAME} emuelec-bluetooth-NoInputNoOutput" && pkill gptokeyb &
+emuelec-bluetooth-NoInputNoOutput
 
-wait
+killall gptokeyb
 
 exit 0
